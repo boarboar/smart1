7922e7f82f1de6500fd174c9ebd3d6e3ba9b9936
Alex Ershov <alex.ershov@gmail.com>
2018-09-07 13:39:47 +0300

    dstest

---
+++ tests/dstest/dstest.ino
@@ -0,0 +1,107 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+#include <OneWire.h>
+
+//#define LED_PIN LED_BUILTIN
+#define LED_PIN 0
+#define OW_PIN 2
+
+OneWire  ds(OW_PIN);  // 
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  delay(2000);
+  Serial.begin(115200);
+  Serial.println(F("INIT"));
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  DS_test();
+  Serial.println(F("TEST"));
+}
+
+void DS_test() {
+  byte i;
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+  
+  if ( !ds.search(addr)) {
+    Serial.print("No more addresses.\n");
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  
+  Serial.print("R=");
+  for( i = 0; i < 8; i++) {
+    Serial.print(addr[i], HEX);
+    Serial.print(" ");
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.print("CRC is not valid!\n");
+      return;
+  }
+  
+  if ( addr[0] != 0x10) {
+      Serial.print("Device is not a DS18S20 family device.\n");
+      return;
+  }
+
+  // The DallasTemperature library can do all this work for you!
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1);         // start conversion, with parasite power on at the end
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  // we might do a ds.depower() here, but the reset will take care of it.
+  
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+
+  Serial.print("P=");
+  Serial.print(present,HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print( OneWire::crc8( data, 8), HEX);
+  Serial.println();
+
+}

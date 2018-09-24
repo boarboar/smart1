063b5691e00acaff8e89988f81e4c769fa487618
Alex Ershov <alex.ershov@gmail.com>
2018-09-24 17:08:54 +0300

    RecyclerView

--- tests/Android/app/src/main/java/com/example/boar/ankotest/MainActivity.kt
+++ tests/Android/app/src/main/java/com/example/boar/ankotest/MainActivity.kt
@@ -5,16 +5,32 @@ import android.os.Bundle
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        /*
         verticalLayout {
             val name = editText()
             button("Say Hello") {
                 onClick { toast("Hello, ${name.text}!") }
             }
-        }
+        }*/
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val list: ArrayList<Movie> = arrayListOf()
+        list.add(Movie("Sherlock Holmes",2009))
+        list.add(Movie("The Shawshank Redemption",1994))
+        list.add(Movie("Forrest Gump",1994))
+        list.add(Movie("Titanic",1997))
+        list.add(Movie("Taxi",1998))
+        list.add(Movie("Inception",1994))
+        list.add(Movie("The Imitation Game",2014))
+
+        recyclerView.adapter = MovieAdapter(list)
     }
 }
---
+++ tests/Android/app/src/main/java/com/example/boar/ankotest/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.boar.ankotest
+
+data class Movie(var title: String, var year: Int)

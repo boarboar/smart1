003192173e774e265331ce5d6c5bc210bf335fb1
Alex Ershov <alex.ershov@gmail.com>
2018-09-26 16:32:08 +0300

    WiFi IP addr

--- tests/Android/app/src/main/java/com/example/boar/ankotest/MainActivity.kt
+++ tests/Android/app/src/main/java/com/example/boar/ankotest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.boar.ankotest
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import org.jetbrains.anko.*
@@ -9,6 +10,34 @@ import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import com.example.boar.ankotest.R.id.recyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Context.WIFI_SERVICE
+import android.net.wifi.WifiInfo
+import android.support.v4.content.ContextCompat.getSystemService
+import android.net.wifi.WifiManager
+import android.text.format.Formatter.formatIpAddress
+
+
+
+private fun getLocalIpAddress(ctx : Context): String? {
+    fun ipToString(i: Int): String {
+        return (i and 0xFF).toString() + "." +
+                (i shr 8 and 0xFF) + "." +
+                (i shr 16 and 0xFF) + "." +
+                (i shr 24 and 0xFF)
+
+    }
+
+    try {
+        val wifiManager: WifiManager = ctx?.getSystemService(WIFI_SERVICE) as WifiManager
+        return ipToString(wifiManager.connectionInfo.ipAddress)
+    } catch (ex: Exception) {
+        Log.e("IP Address", ex.toString())
+    }
+
+    return null
+}
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +66,25 @@ class MainActivity : AppCompatActivity() {
 
         doAsync {
             //var result = runLongTask()
+            /*
+            val wifiMan = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+            val conInfo = wifiMan.getConnectionInfo();
+            val ipAddress = conInfo.getIpAddress();
+
+            //int ipAddress = wifiInf.getIpAddress();
+            //String ip = String.format("%d.%d.%d.%d", (ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff))
+            */
+
+            Log.d("Listener", "Start")
+
+            val ips = getLocalIpAddress(applicationContext) ?: "No Wifi IP"
+
+            Log.d("Listener", ips)
+
+            uiThread {
+                toast(ips)
+            }
+
             for (i in 1..6) {
                 Thread.sleep(5_000)
                 uiThread {

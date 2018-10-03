64930e9b7b2f7d3cab4bc0338f982d4f4065367a
Alex Ershov <alex.ershov@gmail.com>
2018-10-03 19:51:18 +0300

    db

---
+++ tests/AndroidJournaler/app/src/main/java/com/journaler/model/Todo.kt
@@ -0,0 +1,15 @@
+package com.journaler.model
+
+import android.location.Location
+class Todo(
+        title: String,
+        message: String,
+        location: Location,
+        var scheduledFor: Long
+) : Entry(
+        title,
+        message,
+        location
+) {
+    override var id = 0L
+}

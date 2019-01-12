809fcba348be529b98129aa575665f5d8705f231
alexander.ershov <alexander.ershov@gmail.com>
2019-01-12 16:46:42 +0300

    testdht11

---
+++ tests/TestDHT11/TestDHT11.ino
@@ -0,0 +1,40 @@
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+
+int cnt=0;
+
+void setup() {
+  delay(2000);
+  Serial.begin(115200);
+  Serial.println("=================================");
+}
+
+void loop() {
+  // start working...
+   Serial.print(cnt);
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+  
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(5000);
+  cnt++;
+}

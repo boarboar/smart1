6c2469121cc6d38d3a955010b56fb846556f58fb
Alex Ershov <alex.ershov@gmail.com>
2019-11-07 18:30:13 +0300

    fragment overview

---
+++ WeatherApp/app/src/main/java/com/example/android/weatherapp/overview/OverviewViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.android.weatherapp.overview
+
+import androidx.lifecycle.ViewModel
+
+class OverviewViewModel : ViewModel() {
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+
+}

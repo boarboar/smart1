75fbcd3e9d39f4187f3d9bea23318ea57c7379be
Alex Ershov <alex.ershov@gmail.com>
2019-11-11 18:05:29 +0300

    dateutils added

---
+++ WeatherApp/app/src/main/java/com/example/android/weatherapp/utils/DateUtils.kt
@@ -0,0 +1,50 @@
+package com.example.android.weatherapp.utils
+
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import org.threeten.bp.*
+
+class DateUtils {
+    companion object {
+        private val df_time = SimpleDateFormat("HH:mm:ss")
+        private val df_time_short = SimpleDateFormat("HH:mm")
+        private val df_time_honly = SimpleDateFormat("HH")
+        private val df_date_donly = SimpleDateFormat("dd")
+        private val df_date = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+        private var df_dt = DateFormat.getDateTimeInstance( DateFormat.SHORT, DateFormat.SHORT, Locale.GERMAN)
+        // Note: formatters are not threadsafe bit it's no issue since called from UIThread only
+        fun convertDateTime(date: Long = System.currentTimeMillis()) : String {
+            return df_dt.format(date)
+        }
+        fun convertTime(date: Long): String {
+            return df_time.format(date)
+        }
+        fun convertTimeShort(date: Long): String {
+            return df_time_short.format(date)
+        }
+        fun convertTimeHOnly(date: Long): String {
+            return df_time_honly.format(date)
+        }
+        fun convertDateDOnly(date: Long): String {
+            return df_date_donly.format(date)
+        }
+        fun convertDate(date: Long): String {
+            return df_date.format(date)
+        }
+
+        fun millsToLocalDateTime(millis: Long): LocalDateTime {
+            val instant = Instant.ofEpochMilli(millis)
+            return instant.atZone(ZoneId.systemDefault()).toLocalDateTime()
+        }
+
+
+        fun localDateTimeToMillis(ldt: LocalDateTime): Long {
+            val zdt = ldt.atZone(ZoneId.systemDefault())
+            val instant = zdt.toInstant()
+            return instant.toEpochMilli()
+        }
+
+    }
+
+}

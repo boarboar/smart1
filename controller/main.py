5770b6cc75436e1749743693f71810112ce5a1ca
boarboar <alexander.ershov@gmail.com>
2019-12-28 13:22:15 +0300

    json header

--- controller/main.py
+++ controller/main.py
@@ -6,25 +6,31 @@ import socket,select
 from machine import Pin
 
 def handle_http(client, client_addr):
+    led.value(1)
     systime = int(round(time.time() * 1000)) 
     sensors = [
         {'I': 1, 'T': 205, 'V': 3010, 'H' : 900, 'HD' : 2, 'X' : systime},
         {'I': 2, 'T': 215, 'V': 2910, 'H' : 0, 'HD' : 0, 'X' : systime},
         ]
 
-    client.send("HTTP/1.0 200 OK\r\n\r\n %s" % ujson.dumps(sensors))
+    client.send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\n\r\n %s" % ujson.dumps(sensors))
     client.close()
+    led.value(0)
     gc.collect()
 	
-def serv(port=80):
+def serv(port=80, udpport=9998):
     http = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     addr = (socket.getaddrinfo("0.0.0.0", port))[0][-1]
     http.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     http.bind(addr)
     http.listen(4)
-	
     print("Web server started on %s" % str(addr))
- 
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udpaddr = (socket.getaddrinfo("0.0.0.0", udpport))[0][-1]
+    udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    udp.bind(udpaddr)
+    print("UDP server started on %s" % str(udpaddr))
+	
     while True:
         r, w, err = select.select((http,), (), (), 1)
         if r:

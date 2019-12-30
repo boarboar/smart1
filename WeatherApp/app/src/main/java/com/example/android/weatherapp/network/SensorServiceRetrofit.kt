ee5e8352a7eae86031f2fafa9858800c14889dba
Alex Ershov <alex.ershov@gmail.com>
2019-12-30 12:06:02 +0300

    preferences implemented

--- WeatherApp/app/src/main/java/com/example/android/weatherapp/network/SensorServiceRetrofit.kt
+++ WeatherApp/app/src/main/java/com/example/android/weatherapp/network/SensorServiceRetrofit.kt
@@ -1,6 +1,9 @@
 package com.example.android.weatherapp.network
 
+import android.content.Context
+import androidx.preference.PreferenceManager
 import com.example.android.weatherapp.BuildConfig
+import com.example.android.weatherapp.WeatherApplication.Companion.ctx
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -11,15 +14,21 @@ import java.util.concurrent.TimeUnit
 
 object SensorServiceRetrofit {
 
-    //const val BASE_URL = "http://10.0.2.2:8080/"
-    const val BASE_URL = "http://192.168.1.144:80/"
+    const val DEF_BASE_URL = "http://10.0.2.2:8080/"
+    //const val BASE_URL = "http://192.168.1.144:80/"
 
     fun obtain(
             readTimeoutInSeconds: Long = 5,
-            connectTimeoutInSeconds: Long = 10
+            connectTimeoutInSeconds: Long = 15
     ): Retrofit {
+        val sharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(ctx)
+        var url = sharedPreferences.getString("sensor_service_url", DEF_BASE_URL)
+        if(!url.startsWith("http://"))  url = "http://"+url
+        if(!url.endsWith("/"))  url = url + "/"
+
         return Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .client(

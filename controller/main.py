b3a756a023063ac33db0ab6ebdbb35fb8d7a75e3
boarboar <alexander.ershov@gmail.com>
2020-02-01 18:34:52 +0300

    lastv req info

--- controller/main.py
+++ controller/main.py
@@ -11,9 +11,10 @@ html_head = """<html><head><title>ESP32 Web Server</title>
 <body>
 {0} message(s), avg rate {1} s
 <br>
-<br>
+{2} request(s), last one {3} ago
+<br><br>
 <table border="1">
-<tr><th>ID</th><th>Timeout(s)</th></tr>
+<tr><th>ID</th><th>Timeout(sec)</th></tr>
 """
 html_row = "<tr><td>{0}</td><td>{1}</td></tr>"
 html_tail = """
@@ -40,6 +41,8 @@ def format_time(t):
     return "{0} days {1} hours {2} minutes {3} seconds".format(d, h, m, s)
 
 def handle_http(client, client_addr):    
+    global rc
+    global req_time
     led.value(1)
     try :
         now = time.time()
@@ -49,10 +52,14 @@ def handle_http(client, client_addr):
             print("Recv HTTP: %s" % sdata)
             if sdata.find('/status') != -1 :
                 client.send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n ")
-                if mc !=0 :
-                    client.send(html_head.format(str(mc), str(int( round((time.time()-start_time)/mc) )) ))
+                if req_time != None :
+                    req_time_str = format_time(now-req_time)
                 else :
-                    client.send(html_head.format(str(mc), "-"))    
+                    req_time_str = "-"    
+                if mc !=0 :
+                    client.send(html_head.format(str(mc), str(int( round((time.time()-start_time)/mc) )), str(rc), req_time_str ))
+                else :
+                    client.send(html_head.format(str(mc), "-", str(rc), req_time_str))    
                 for s in list(sensor_data.values()) :
                     client.send(html_row.format(str(s['I']), str(int(round((now*1000-s['X'])/1000)))  ))
                 client.send(html_tail.format( format_time(now),  format_time(now-start_time), gc.mem_free(), str(int(round(time.time()-now))) ))
@@ -60,6 +67,8 @@ def handle_http(client, client_addr):
                 client.send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n\r\n ")
                 #client.send(json.dumps(list(sensor_data.values())))
                 client.send(json.dumps(list(filter(lambda s: now*1000-s['X'] < 3600000, sensor_data.values()))))
+                rc = rc + 1
+                req_time = now
         client.close()
     except Exception as e:
         print('HTTP handler error: %s' % str(e)) 
@@ -186,7 +195,9 @@ gc.collect()
 sensor_data = {}
 
 start_time = time.time()
-mc = 0   
+req_time = None
+mc = 0
+rc = 0   
         
 wdt = WDT(timeout=60000)  # enable it with a timeout of 1 min
 

c609f35553994f2a9f22d168a8d4ed8956e79429
boarboar <alexander.ershov@gmail.com>
2020-02-02 19:22:54 +0300

    room db explicit journal mode

--- WeatherApp/app/src/main/java/com/example/android/weatherapp/database/Database.kt
+++ WeatherApp/app/src/main/java/com/example/android/weatherapp/database/Database.kt
@@ -248,7 +248,7 @@ fun getDatabase(context: Context): WeatherDatabase {
                 WeatherDatabase::class.java,
                 "weather")
                 //.addMigrations(MIGRATION_1_2, MIGRATION_2_3)
-                .setJournalMode(RoomDatabase.JournalMode.WRITE_AHEAD_LOGGING)  // TRUNCATE WRITE_AHEAD_LOGGING // AUTOMATIC
+                .setJournalMode(RoomDatabase.JournalMode.TRUNCATE)  // TRUNCATE WRITE_AHEAD_LOGGING // AUTOMATIC
                 .build()
         }
     }
